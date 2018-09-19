1750356d55a6af1e7bdd363bcdca1968f242aa92
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-20 08:23:56 +1000

    add particles background to hero section

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,12 @@ $(function() {
 
   $("#hamburger").click(function(){
     console.log("Clicked")
-    $("#nav-list").toggleClass('hidden');
+    $("#nav-list").toggleClass('open');
+    $("#hamburger").toggleClass('open');
   });
 
+});
+
+particlesJS.load('particles-js', 'https://api.myjson.com/bins/1c12jw', function() {
+  console.log('callback - particles.js config loaded');
 });
