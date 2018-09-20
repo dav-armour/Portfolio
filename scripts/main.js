b919d4c42da47d9de3bb7318ff9311c01111c5b8
dav-armour <15181647+dav-armour@users.noreply.github.com>
2018-09-20 11:49:15 +1000

    add about and contact sections

--- scripts/main.js
+++ scripts/main.js
@@ -6,8 +6,22 @@ $(function() {
     $("#hamburger").toggleClass('open');
   });
 
+  $("a").on('click', function(event) {
+    $("#nav-list").toggleClass('open');
+    $("#hamburger").toggleClass('open');
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    };
+  });
+
 });
 
-particlesJS.load('particles-js', 'https://api.myjson.com/bins/1c12jw', function() {
+  particlesJS.load('particles-js', 'https://api.myjson.com/bins/1c12jw', function() {
   console.log('callback - particles.js config loaded');
 });
